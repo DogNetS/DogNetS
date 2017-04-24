0d20875db6dcf92be8a1dd3e45c689f5f6c97ad8
Alexander Hoang <maxelnot@gmail.com>
2017-04-23 17:13:44 -0700

    segues in progress

--- DogNet/DogNet/DogSignup1ViewController.swift
+++ DogNet/DogNet/DogSignup1ViewController.swift
@@ -21,6 +21,9 @@ class DogSignup1ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func dogAdd(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
