7e407ae2e3ba1e092435c8a0252116c675e048d7
Sean Nam <namsangwoo1@gmail.com>
2017-05-17 11:49:35 -0700

    user updated in HomeViewController

--- DogNet/DogNet/HomeViewController.swift
+++ DogNet/DogNet/HomeViewController.swift
@@ -13,6 +13,17 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     var PFDogs: [PFObject]!
     var dogs: [Dog]!
+    var user_data: [PFObject]!
+    var name: String?
+    var age:Int = 0
+    var num_dogs:Int = 0
+    var bioText:String = ""
+    var location:String = ""
+    let loadingIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
     
     @IBOutlet weak var tableView: UITableView!
     @IBAction func onLogout(_ sender: Any) {
@@ -52,6 +63,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        /*  Get dog data from Parse */
         var query = PFQuery(className: "dog_data")
         query.order(byDescending: "createdAt")
         query.includeKey("owner")
@@ -68,6 +80,33 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             } else {
                 // Log details of the failure
                 print("error")
+            }
+        }
+        
+        /*  Get user_data from Parse */
+        // clear fields while data is loading
+        self.profileImageView.image = nil
+        self.nameLabel.text = ""
+        self.locationLabel.text = ""
+        
+        // search for PFObject with matching "owner" field
+        query = PFQuery(className: "user_data")
+        query.order(byDescending: "createdAt")
+        query.includeKey("owner")
+        query.whereKey("owner", equalTo: PFUser.current()!)
+        query.limit = 20
+        query.findObjectsInBackground { (user_data: [PFObject]?, error: Error?) -> Void in
+            if let data = user_data {
+                self.user_data = data
+                
+                print("user data: \(self.user_data)")
+                    
+                self.saveData()                         // save data from parse to local variables
+                self.updateTextLabels()                 // update text labels with new values
+                self.loadingIndicator.stopAnimating()   // stop activity indicator and show updated views
+                
+            } else {
+                print("error while getting user_data")
             }
         }
 
@@ -120,6 +159,41 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
 
+    // saves the data from parse locally
+    func saveData() {
+        
+        if(self.user_data?[0]["name"] != nil) {
+            self.name = self.user_data?[0]["name"] as? String
+        } else {
+            self.name = (PFUser.current()?.username)! as String
+        }
+        
+        if(self.user_data?[0]["location"] != nil) {
+            self.location = self.user_data?[0]["location"] as! String
+        } else {
+            self.location = "Location not found"
+        }
+        
+    }
+    
+    func updateTextLabels() {
+        self.nameLabel.text = self.name
+        self.locationLabel.text = self.location
+        
+        if(self.user_data?[0]["profilePic"] != nil) {
+            if let userPic = user_data[0].value(forKey: "profilePic")! as? PFFile {
+                userPic.getDataInBackground({ (imageData: Data?, error: Error?) -> Void in
+                    let image = UIImage(data: imageData!)
+                    if image != nil {
+                        self.profileImageView.image = image!
+                    }
+                })
+            }
+        } else {
+            // set default profile image
+            self.profileImageView.image =  UIImage(named: "profile_avatar")
+        }
+    }
     
     
     // MARK: - Navigation
